7bbba66c2b31951051d65191e6d516fdcbbffe33
juanobligado <juan.obligado@gmail.com>
2024-10-28 22:23:30 -0300

    Only filter Uni swap transactions from mem pool

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::error::Error;
+use std::str::FromStr;
+use alloy::consensus::Transaction;
+use alloy::primitives::{address, Address};
 use alloy::providers::{Provider, ProviderBuilder, WsConnect};
 use eyre::Result;
 use futures_util::StreamExt;
@@ -15,14 +18,29 @@ async fn main() -> Result<()> {
 
     // Wait and take the next 4 blocks.
     let mut stream = sub.into_stream();
-
+    const UNISWAP_V2FACTORY_ADDRESS: Address = address!("3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad");
     println!("Listening for pending transactions...");
     // Take the stream and print the pending transaction.
     let handle = tokio::spawn(async move {
         while let Some(tx_hash) = stream.next().await {
             // Get the transaction details.
             if let Ok(tx) = provider.get_transaction_by_hash(tx_hash).await {
-                println!("Transaction details: {tx:#?}");
+                match tx {
+                    Some(tx) => {
+                        match tx.to() {
+                            Some(to) => {
+                                if to == UNISWAP_V2FACTORY_ADDRESS {
+                                    println!("Uniswap Transaction: {:?}", tx);
+                                }
+                            },
+                            None => {
+                                println!("Skipping Transaction {} ", tx_hash);
+                            }
+                        }
+                    },
+                    None => { }
+                }
+
             }
         }
     });
